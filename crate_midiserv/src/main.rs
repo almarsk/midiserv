3ff63f3be4373d8996c71d21a10d102459bff992
Albert Maršík <albert.marsik@gmail.com>
2024-12-09 14:04:28 +0100

    server draft #WIP

--- crate_midiserv/src/main.rs
+++ crate_midiserv/src/main.rs
@@ -1,23 +1,18 @@
 use axum::extract::{ws::WebSocket, ConnectInfo};
-use axum::handler::HandlerWithoutStateExt;
 use axum::http::{StatusCode, Uri};
-use axum::routing::{any_service, MethodRouter};
 use axum::{extract::WebSocketUpgrade, response::IntoResponse, routing::get, Router};
 use std::net::SocketAddr;
-use tower_http::cors::{Any, CorsLayer};
 use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let cors = CorsLayer::new().allow_origin(Any);
     let app = Router::new()
         .fallback(fallback)
-        .nest_service("/static", serve_dir("static".to_string()))
-        .nest_service("/assets", serve_dir("assets".to_string()))
-        .route("/ws", get(ws_handler))
-        .layer(cors);
+        .nest_service("/", ServeDir::new("static"))
+        .nest_service("/assets", ServeDir::new("assets"))
+        .route("/ws", get(ws_handler));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:5000")
         .await
         .unwrap();
 
@@ -27,14 +22,6 @@ async fn main() {
     )
     .await
     .unwrap();
-}
-
-pub fn serve_dir(web_folder: String) -> MethodRouter {
-    async fn handle_404() -> (StatusCode, &'static str) {
-        (StatusCode::NOT_FOUND, "Resource not found.")
-    }
-
-    any_service(ServeDir::new(web_folder).not_found_service(handle_404.into_service()))
 }
 
 async fn fallback(uri: Uri) -> (StatusCode, String) {
--- update_frontend.sh
+++ update_frontend.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-DEST_PATH="../crate_midiserv/static/"
+cd frontend
+
+DEST_PATH="../crate_midiserv/static"
 
 # Check if the destination directory exists, if not create it
 if [ ! -d "$DEST_PATH" ]; then
     mkdir -p "$DEST_PATH"
 fi
 
-# Build the frontend using npm
 npm run build
 
-# Copy the new build files to the destination directory
 cp -r dist/index.html "$DEST_PATH"
 cp -r dist/assets ../crate_midiserv
+
+cd ..
 
 echo "Frontend build updated successfully!"
